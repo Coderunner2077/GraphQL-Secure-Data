78c480a54376c81b38fb3b3449d0778ebf57565d
Coderunner2077 <coderunner2077@gmail.com>
2022-09-27 00:26:22 +0200

    Catch and return error in case of error thrown

--- index.ts
+++ index.ts
@@ -1,20 +1,25 @@
+import { QueryValidationError } from "./error";
 import { GraphQLResolveInfo } from "graphql";
 import { AllowedFields } from "./types";
 import { accessControl } from "./utils/control";
 
 /**
- * Middleware implementing full access control on fields of objects returned by the incoming GraphQL
- * operations. If an object is entered with its fields, it means that any query, mutation or subscription
+ * Middleware implementing full access control on fields of data types returned by the incoming GraphQL
+ * operations. If a data type is entered with its fields, it means that any query, mutation or subscription
  * will have to limit its data fetching to those fields, in other words they can fetch less but not more.
- * Furthemore, if a data object is not included in this middleware, there will be no restrictions on it as
+ * Furthemore, if a data type is not included in this middleware, there will be no restrictions on it as
  * far as this middleware is concerned
  *
- * @param fields AllowedFields
+ * @param fieldsTree AllowedFields
  * @returns resolver
  */
-export const secure = (fields: Record<string, string | (string | AllowedFields)[]>) => {
+export const secure = (fieldsTree: AllowedFields) => {
     return (resolve: Function, root: any, args: any, ctx: any, info: GraphQLResolveInfo) => {
-        accessControl(info, fields);
+        try {
+            accessControl(info, fieldsTree);
+        } catch (err) {
+            return err;
+        }
         return resolve(root, args, ctx, info);
     }
 }
